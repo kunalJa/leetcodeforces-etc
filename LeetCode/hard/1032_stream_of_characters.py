0f2c7efb6a297f9ce92908c6f9e6596eaeb04726
kunalja <kunaljain@utexas.edu>
2019-10-19 16:14:31 -0500

    Leetcode Hard Aho-Corasick

--- LeetCode/hard/1032_stream_of_characters.py
+++ LeetCode/hard/1032_stream_of_characters.py
@@ -28,23 +28,25 @@ from collections import deque
 
 class StreamChecker:
     class Node:
-        def __init__(self, val: str, parent: "Node", real: bool):
+        def __init__(self, val: str, parent: "Node", real: bool, path: str):
             self.val = val
             self.parent = parent
             self.real = real
-            self.children = None
+            self.path = path
+            self.children = {}
             self.longest_strict_suffix_link = None
             self.valid_suffix_link = None
 
     def __init__(self, words: List[str]):
         self.trie = self.__build_trie(words)
-        __add_suffix_links(self, self.trie)
+        StreamChecker.__add_suffix_links(self.trie)
+        self.current = self.trie
 
-    def __print_trie(self) -> None:
+    @staticmethod
+    def __print_trie(root: Node) -> None:
         """Prints the trie in level order, each level on a new line."""
         q = deque()
-        seen = set()
-        q.append(self.trie)
+        q.append(root)
         q.append(None)
         while q:
             current = q.popleft()
@@ -59,7 +61,7 @@ class StreamChecker:
     def __build_trie(self, words: List[str]) -> Node:
         """Returns a trie from the input list of words."""
         words = set(words)
-        root = self.Node("", parent=None, real=False)
+        root = self.Node("", parent=None, real=False, path="")
 
         for word in words:
             current = root
@@ -67,11 +69,12 @@ class StreamChecker:
             for char in word:
                 path.append(char)
                 word_so_far = "".join(path)
-                if not current.children:
-                    current.children = {}
                 if char not in current.children:
                     current.children[char] = self.Node(
-                        char, parent=current, real=(word_so_far in words)
+                        char,
+                        parent=current,
+                        real=(word_so_far in words),
+                        path=word_so_far,
                     )
                 current = current.children[char]
 
@@ -79,11 +82,59 @@ class StreamChecker:
 
     @staticmethod
     def __add_suffix_links(root: Node) -> None:
-        """Adds suffix links to to the passed in trie."""
-        pass
+        """
+        Adds suffix links to to the passed in trie.
+        The blue arcs can be computed in linear time by repeatedly traversing the blue arcs until
+        the traversing node has a child matching the character of the target node.
+        """
+        q = deque()
+        q.append(root)
+        while q:
+            current = q.popleft()
+            parent = current.parent
+            while parent and not current.longest_strict_suffix_link:
+                blue_link = parent.longest_strict_suffix_link
+                if blue_link:
+                    if current.val in blue_link.children:
+                        current.longest_strict_suffix_link = blue_link.children[
+                            current.val
+                        ]
+                else:
+                    current.longest_strict_suffix_link = parent
+                parent = blue_link
+
+            q.extend(current.children.values())
+
+        q.append(root)
+        while q:
+            current = q.popleft()
+            blue_link = current.longest_strict_suffix_link
+            while blue_link and not current.valid_suffix_link:
+                if blue_link.real:
+                    current.valid_suffix_link = blue_link
+                elif blue_link.valid_suffix_link:
+                    current.valid_suffix_link = blue_link.valid_suffix_link
+
+                blue_link = blue_link.longest_strict_suffix_link
+
+            q.extend(current.children.values())
 
     def query(self, letter: str) -> bool:
-        return True
+        if letter in self.current.children:
+            self.current = self.current.children[letter]
+        else:
+            while self.current.longest_strict_suffix_link:
+                if letter in self.current.children:
+                    self.current = self.current.children[letter]
+                    break
+
+                self.current = self.current.longest_strict_suffix_link
+
+            if self.current.val != letter:
+                if letter in self.current.children:
+                    self.current = self.current.children[letter]
+
+        return self.current.real or self.valid_suffix_link
 
 
 # Your StreamChecker object will be instantiated and called as such:
